93817bab3f6eb2ad6e1c2d6cd5ceaa768ceddccc
Daniel Klevebring <daniel.klevebring@gmail.com>
2014-10-23 19:53:07 +0200

    bugfix getReports

--- R/getReports.R
+++ R/getReports.R
@@ -8,17 +8,18 @@
 getReports <- function(path, recursive=TRUE){
   ## get list of all report files
   reportFiles <- dir(path = path, pattern="report$", recursive=recursive)
-  
+  prefix <- paste(path, dirname(reportFiles), sep="")
   ## set up empty data frames
-  reports <- read.table(reportFiles[1], sep="\t", header=TRUE, stringsAsFactors=FALSE)
+  reports <- read.table(paste(path,reportFiles[1],sep="/"), sep="\t", header=TRUE, stringsAsFactors=FALSE)
   reports <- reports[-1,]
   
   ## read reports
   reportFile <- reportFiles[10]
   for(reportFile in reportFiles){ # 
-    report <- read.table(reportFile, header=TRUE, stringsAsFactors=FALSE)
+    report <- read.table(paste(path,reportFile, sep="/"), header=TRUE, stringsAsFactors=FALSE)
     reports <- rbind(reports, report)
   }
+  reports$prefix <- prefix
   reports
 }
 
