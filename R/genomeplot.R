1a8854827339226bb7386922a2ef3d5a6e25074b
Daniel Klevebring <daniel.klevebring@gmail.com>
2014-10-27 15:26:11 +0100

    hard coded genome end

--- R/genomeplot.R
+++ R/genomeplot.R
@@ -42,7 +42,7 @@ genomeplot <- function(segments, chrsizes){
     annotate("text", x=as.numeric(chrsizes$labelpos), y=-1.3, label=chrsizes$chr, size=4, colour="gray50") +
     theme(axis.text.x=element_blank(),axis.ticks.x=element_blank(), panel.grid=element_blank()) + 
     ggtitle("Copy Number Profile") + 
-    geom_segment(aes(x = 0, y = 0, xend = max(chrsizes$cumend[ is.finite(chrsizes$cumend)] ), yend = 0), 
+    geom_segment(aes(x = 0, y = 0, xend = 2881033286, yend = 0), 
                  size=.2, colour="gray10")
   )
   #annotate("text", x=max(segments$cumend)+1e7, y=log2(c(1/2, 3/2, 4/2, 5/2, 6/2)), 
