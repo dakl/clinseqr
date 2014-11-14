652fd94f8c59e16ad548cabbca3b3fdeb4ca32a5
Daniel Klevebring <daniel.klevebring@gmail.com>
2014-11-14 15:27:46 +0100

    Added getQDNAseq.R

---
+++ R/getQDNAseq.R
@@ -0,0 +1,32 @@
+#' Get segmented CNV data
+#' 
+#' @param reports data frame with reports
+#' @param columnToUse column to use for getting values
+#' @return A data frame with segmented value, or other value
+#' @examples
+#' #dat <- getReadcountPerChr(reports)
+#' #dat <- getReadcountPerChr(reports, columnToUse="segmented")
+getQDNAseq <- function(reports, columnToUse="segmented"){
+  dat <- makeEmptyDataTable(header = c("chr", "start", "end", "gc", "mappability"))
+  for(k in 1:nrow(reports)){ #k <- 3
+    infile   <- paste(reports$prefix[k] ,reports$QDNAseq[k],sep="/")
+    if(file.exists(infile)){
+      tb <- fread(infile)
+      dat <- rbindlist(list(dat, data.table(tb$chromosome, tb$start, tb$end, tb$gc, tb$mappability)))
+      break
+    }
+  }
+  
+  for(k in 1:nrow(reports)){ #k <- 3
+    infile   <- paste(reports$prefix[k] ,reports$QDNAseq[k],sep="/")
+    if(file.exists(infile)){
+      tb <- fread(infile)
+      dat[, eval(reports$DataReportID[k]) := tb[,columnToUse, with=FALSE ] ]
+    }else{
+      dat[, eval(reports$DataReportID[k]):=NA ]
+    }
+    dot(k, every=10)
+  }
+  dat
+}
+
