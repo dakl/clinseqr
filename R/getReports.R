a8d4da1b9c325d620be624feca2f364cfadbf42f
Daniel Klevebring <daniel.klevebring@gmail.com>
2015-10-06 16:08:39 +0200

    made it possible to get absolute paths from getReports()

--- R/getReports.R
+++ R/getReports.R
@@ -2,24 +2,44 @@
 #' 
 #' @param path Path to directory with reports. 
 #' @param recursive Boolean whether or not to recurse when looking for report files (default TRUE)
+#' @param absolutepaths Boolean whether or not to make paths absolute paths instead of relative (default FALSE)
 #' @return A data frame with reports
 #' @examples
-#' # getReports("/home/Crisp/clinseq/BREASTv4")
-getReports <- function(path, recursive=TRUE){
+#' reports1 <- getReports(path="/proj/b2010040/private/clinseq/LAMLv6/", recursive=TRUE, absolutepaths=TRUE, max=10)
+#' reports2 <- getReports(path="/proj/b2010040/private/clinseq/bla/", recursive=TRUE, absolutepaths=TRUE, max=10)
+getReports <- function(path, recursive=TRUE, absolutepaths=FALSE, max){
   ## get list of all report files
   reportFiles <- dir(path = path, pattern="report$", recursive=recursive)
-  prefix <- paste(path, dirname(reportFiles), sep="/")
-  ## set up empty data frames
-  reports <- read.table(paste(path,reportFiles[1],sep="/"), sep="\t", header=TRUE, stringsAsFactors=FALSE)
-  reports <- reports[-1,]
-  
-  ## read reports
-  reportFile <- reportFiles[10]
-  for(reportFile in reportFiles){ # 
-    report <- read.table(paste(path,reportFile, sep="/"), header=TRUE, stringsAsFactors=FALSE)
-    reports <- rbind(reports, report)
+  if(length(reportFiles) == 0){ ## if there aren't any report files in path, return NULL
+    return(NULL)
+  } else{
+    prefix <- paste(path, dirname(reportFiles), sep="/")
+    ## set up empty data frames
+    reports <- read.table(paste(path,reportFiles[1],sep="/"), sep="\t", header=TRUE, stringsAsFactors=FALSE)
+    reports$prefix <- NA
+    reports <- reports[-1,]
+    
+    ## read reports
+    for(k in 1:length(reportFiles)){ # 
+      if( k > max ){
+        break
+      }
+      reportFile <- reportFiles[k]
+      report <- read.table(paste(path,reportFile, sep="/"), header=TRUE, stringsAsFactors=FALSE)
+      
+      if(absolutepaths){
+        for( n in 11:length(report)){
+          if(!is.na(report[n])){
+            report[n] <- paste(prefix[k], report[n], sep="/")
+          }
+        }        
+      }
+      
+      report$prefix <- prefix[k]
+      reports <- rbind(reports, report)
+    }
+    
+    reports
   }
-  reports$prefix <- prefix
-  reports
 }
 
